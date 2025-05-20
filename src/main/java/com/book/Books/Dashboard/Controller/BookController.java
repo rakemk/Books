e1be28711c8c508af31ef6382c29334350188b84
rakemk <makwanarakesh219@gmail.com>
2025-05-20 19:36:38 +0530

    add book api done

---
+++ src/main/java/com/book/Books/Dashboard/Controller/BookController.java
@@ -0,0 +1,36 @@
+package com.book.Books.Dashboard.Controller;
+
+import com.book.Books.Dashboard.Dto.BookDto;
+import com.book.Books.Dashboard.Service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+    @Autowired
+    private BookService bookService;
+
+    @PostMapping
+    public BookDto addBook(@RequestBody BookDto bookDto){
+        return bookService.addBook(bookDto);
+    }
+
+    @GetMapping
+    public List<BookDto> getAllBooks(){
+        return bookService.getAllBooks();
+    }
+
+    @PutMapping("/{id}")
+    public BookDto updateBook(@PathVariable Long id,@RequestBody BookDto bookDto){
+        return bookService.updateBook(id, bookDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteBook(@PathVariable Long id){
+        bookService.deleteBook(id);
+    }
+}
---
+++ src/main/java/com/book/Books/Dashboard/Repository/BookRepository.java
@@ -0,0 +1,10 @@
+package com.book.Books.Dashboard.Repository;
+
+import com.book.Books.Dashboard.Entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+
+}
