11807af82e3198a3d7e9337007d413b910fdc872
Nileshrathod21 <im.nileshrathod@gmail.com>
2025-06-02 18:01:52 +0530

    all up todate

--- src/main/java/com/book/Books/Dashboard/controller/UserController.java
+++ src/main/java/com/book/Books/Dashboard/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 @RestController
 @RequestMapping("/users")
+@CrossOrigin(value = "*")
 public class UserController {
     @Autowired
     private UserService userService;
